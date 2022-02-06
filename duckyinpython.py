2c7b722fe22544a140ed35350a8544ef16940b62
Craig <me@craigwilson.blog>
2022-02-06 18:43:26 +1100

    Update duckyinpython.py
    Updated issue on live system

--- duckyinpython.py
+++ duckyinpython.py
@@ -11,39 +11,94 @@ from adafruit_hid.keycode import Keycode
 
 # uncomment these lines for non_US keyboards
 # replace LANG with appropriate language
-#from keyboard_layout_win_LANG import KeyboardLayout
-#from keycode_win_LANG import Keycode
+# from keyboard_layout_win_LANG import KeyboardLayout
+# from keycode_win_LANG import Keycode
 
 import time
 import digitalio
-from board import *
-led = digitalio.DigitalInOut(LED)
+import board
+
+# from board import *
+
+led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 duckyCommands = {
-    'WINDOWS': Keycode.WINDOWS, 'GUI': Keycode.GUI,
-    'APP': Keycode.APPLICATION, 'MENU': Keycode.APPLICATION, 'SHIFT': Keycode.SHIFT,
-    'ALT': Keycode.ALT, 'CONTROL': Keycode.CONTROL, 'CTRL': Keycode.CONTROL,
-    'DOWNARROW': Keycode.DOWN_ARROW, 'DOWN': Keycode.DOWN_ARROW, 'LEFTARROW': Keycode.LEFT_ARROW,
-    'LEFT': Keycode.LEFT_ARROW, 'RIGHTARROW': Keycode.RIGHT_ARROW, 'RIGHT': Keycode.RIGHT_ARROW,
-    'UPARROW': Keycode.UP_ARROW, 'UP': Keycode.UP_ARROW, 'BREAK': Keycode.PAUSE,
-    'PAUSE': Keycode.PAUSE, 'CAPSLOCK': Keycode.CAPS_LOCK, 'DELETE': Keycode.DELETE,
-    'END': Keycode.END, 'ESC': Keycode.ESCAPE, 'ESCAPE': Keycode.ESCAPE, 'HOME': Keycode.HOME,
-    'INSERT': Keycode.INSERT, 'NUMLOCK': Keycode.KEYPAD_NUMLOCK, 'PAGEUP': Keycode.PAGE_UP,
-    'PAGEDOWN': Keycode.PAGE_DOWN, 'PRINTSCREEN': Keycode.PRINT_SCREEN, 'ENTER': Keycode.ENTER,
-    'SCROLLLOCK': Keycode.SCROLL_LOCK, 'SPACE': Keycode.SPACE, 'TAB': Keycode.TAB,
-    'BACKSPACE': Keycode.BACKSPACE,
-    'A': Keycode.A, 'B': Keycode.B, 'C': Keycode.C, 'D': Keycode.D, 'E': Keycode.E,
-    'F': Keycode.F, 'G': Keycode.G, 'H': Keycode.H, 'I': Keycode.I, 'J': Keycode.J,
-    'K': Keycode.K, 'L': Keycode.L, 'M': Keycode.M, 'N': Keycode.N, 'O': Keycode.O,
-    'P': Keycode.P, 'Q': Keycode.Q, 'R': Keycode.R, 'S': Keycode.S, 'T': Keycode.T,
-    'U': Keycode.U, 'V': Keycode.V, 'W': Keycode.W, 'X': Keycode.X, 'Y': Keycode.Y,
-    'Z': Keycode.Z, 'F1': Keycode.F1, 'F2': Keycode.F2, 'F3': Keycode.F3,
-    'F4': Keycode.F4, 'F5': Keycode.F5, 'F6': Keycode.F6, 'F7': Keycode.F7,
-    'F8': Keycode.F8, 'F9': Keycode.F9, 'F10': Keycode.F10, 'F11': Keycode.F11,
-    'F12': Keycode.F12,
-
+    "WINDOWS": Keycode.WINDOWS,
+    "GUI": Keycode.GUI,
+    "APP": Keycode.APPLICATION,
+    "MENU": Keycode.APPLICATION,
+    "SHIFT": Keycode.SHIFT,
+    "ALT": Keycode.ALT,
+    "CONTROL": Keycode.CONTROL,
+    "CTRL": Keycode.CONTROL,
+    "DOWNARROW": Keycode.DOWN_ARROW,
+    "DOWN": Keycode.DOWN_ARROW,
+    "LEFTARROW": Keycode.LEFT_ARROW,
+    "LEFT": Keycode.LEFT_ARROW,
+    "RIGHTARROW": Keycode.RIGHT_ARROW,
+    "RIGHT": Keycode.RIGHT_ARROW,
+    "UPARROW": Keycode.UP_ARROW,
+    "UP": Keycode.UP_ARROW,
+    "BREAK": Keycode.PAUSE,
+    "PAUSE": Keycode.PAUSE,
+    "CAPSLOCK": Keycode.CAPS_LOCK,
+    "DELETE": Keycode.DELETE,
+    "END": Keycode.END,
+    "ESC": Keycode.ESCAPE,
+    "ESCAPE": Keycode.ESCAPE,
+    "HOME": Keycode.HOME,
+    "INSERT": Keycode.INSERT,
+    "NUMLOCK": Keycode.KEYPAD_NUMLOCK,
+    "PAGEUP": Keycode.PAGE_UP,
+    "PAGEDOWN": Keycode.PAGE_DOWN,
+    "PRINTSCREEN": Keycode.PRINT_SCREEN,
+    "ENTER": Keycode.ENTER,
+    "SCROLLLOCK": Keycode.SCROLL_LOCK,
+    "SPACE": Keycode.SPACE,
+    "TAB": Keycode.TAB,
+    "BACKSPACE": Keycode.BACKSPACE,
+    "A": Keycode.A,
+    "B": Keycode.B,
+    "C": Keycode.C,
+    "D": Keycode.D,
+    "E": Keycode.E,
+    "F": Keycode.F,
+    "G": Keycode.G,
+    "H": Keycode.H,
+    "I": Keycode.I,
+    "J": Keycode.J,
+    "K": Keycode.K,
+    "L": Keycode.L,
+    "M": Keycode.M,
+    "N": Keycode.N,
+    "O": Keycode.O,
+    "P": Keycode.P,
+    "Q": Keycode.Q,
+    "R": Keycode.R,
+    "S": Keycode.S,
+    "T": Keycode.T,
+    "U": Keycode.U,
+    "V": Keycode.V,
+    "W": Keycode.W,
+    "X": Keycode.X,
+    "Y": Keycode.Y,
+    "Z": Keycode.Z,
+    "F1": Keycode.F1,
+    "F2": Keycode.F2,
+    "F3": Keycode.F3,
+    "F4": Keycode.F4,
+    "F5": Keycode.F5,
+    "F6": Keycode.F6,
+    "F7": Keycode.F7,
+    "F8": Keycode.F8,
+    "F9": Keycode.F9,
+    "F10": Keycode.F10,
+    "F11": Keycode.F11,
+    "F12": Keycode.F12,
 }
+
+
 def convertLine(line):
     newline = []
     # print(line)
@@ -64,33 +119,36 @@ def convertLine(line):
     # print(newline)
     return newline
 
+
 def runScriptLine(line):
     for k in line:
         kbd.press(k)
     kbd.release_all()
 
+
 def sendString(line):
     layout.write(line)
 
+
 def parseLine(line):
     global defaultDelay
-    if(line[0:3] == "REM"):
+    if line[0:3] == "REM":
         # ignore ducky script comments
         pass
-    elif(line[0:5] == "DELAY"):
-        time.sleep(float(line[6:])/1000)
-    elif(line[0:6] == "STRING"):
+    elif line[0:5] == "DELAY":
+        time.sleep(float(line[6:]) / 1000)
+    elif line[0:6] == "STRING":
         sendString(line[7:])
-    elif(line[0:5] == "PRINT"):
+    elif line[0:5] == "PRINT":
         print("[SCRIPT]: " + line[6:])
-    elif(line[0:6] == "IMPORT"):
+    elif line[0:6] == "IMPORT":
         runScript(line[7:])
-    elif(line[0:13] == "DEFAULT_DELAY"):
+    elif line[0:13] == "DEFAULT_DELAY":
         defaultDelay = int(line[14:]) * 10
-    elif(line[0:12] == "DEFAULTDELAY"):
+    elif line[0:12] == "DEFAULTDELAY":
         defaultDelay = int(line[13:]) * 10
-    elif(line[0:3] == "LED"):
-        if(led.value == True):
+    elif line[0:3] == "LED":
+        if led.value:
             led.value = False
         else:
             led.value = True
@@ -98,16 +156,17 @@ def parseLine(line):
         newScriptLine = convertLine(line)
         runScriptLine(newScriptLine)
 
+
 kbd = Keyboard(usb_hid.devices)
 layout = KeyboardLayout(kbd)
 
 # sleep at the start to allow the device to be recognized by the host computer
-time.sleep(.5)
+time.sleep(0.5)
 
 # check GP0 for setup mode
 # see setup mode for instructions
 progStatus = False
-progStatusPin = digitalio.DigitalInOut(GP0)
+progStatusPin = digitalio.DigitalInOut(board.GP0)
 progStatusPin.switch_to_input(pull=digitalio.Pull.UP)
 progStatus = not progStatusPin.value
 defaultDelay = 0
@@ -117,34 +176,40 @@ btn_attack = digitalio.DigitalInOut(board.GP18)
 btn_attack.direction = digitalio.Direction.INPUT
 btn_attack.pull = digitalio.Pull.DOWN
 
+
 def runScript(file):
     global defaultDelay
 
     duckyScriptPath = file
-    f = open(duckyScriptPath,"r",encoding='utf-8')
+    f = open(duckyScriptPath, "r", encoding="utf-8")
     previousLine = ""
     duckyScript = f.readlines()
     for line in duckyScript:
         line = line.rstrip()
-        if(line[0:6] == "REPEAT"):
+        if line[0:6] == "REPEAT":
             for i in range(int(line[7:])):
-                #repeat the last command
+                # repeat the last command
                 parseLine(previousLine)
-                time.sleep(float(defaultDelay)/1000)
+                time.sleep(float(defaultDelay) / 1000)
         else:
             parseLine(line)
             previousLine = line
-        time.sleep(float(defaultDelay)/1000)
+        time.sleep(float(defaultDelay) / 1000)
 
 
-print("Running press button to fire attack")
+print("Running press button to fire attack")cmd
+
 while True:
     if btn_attack.value:
-        if(progStatus == False):
-            # not in setup mode, inject the payload
-            print("Running payload.dd")
-            runScript("payload.dd")
-    
-            print("Done")
-        else:
-            print("Update your payload")
+        led.value = True
+        time.sleep(0.5)
+        led.value = False
+        time.sleep(0.5)
+        led.value = True
+        time.sleep(0.5)
+        led.value = False
+        time.sleep(0.5)cmd
+        print("Running payload.dd")
+        runScript("payload.dd")
+        print("Done")
+        
